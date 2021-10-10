87e640a9158511541da6a9361bcd21b00193cb12
Eddishot <Eddi_sl94@hotmail.it>
2021-10-10 22:14:48 +0200

    exercise completed

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,54 @@
 
+console.log("check");
 
-const distanzaKm = prompt("Quanti chilometri vuole percorrere?");
+const distanzaKm = parseInt (prompt("Quanti chilometri vuole percorrere?"));
 
-const etaPasseggero = prompt("Quanti anni ha?");
+if (isNaN(distanzaKm)){
+
+    alert("Errore.Immettere solo cifre numeriche");
+
+} else{
+    console.log("valido");
+}
+
+
+const etaPasseggero = parseInt (prompt("Quanti anni ha?"));
+
+if (isNaN(etaPasseggero)){
+
+    alert("Errore.Immettere solo cifre numeriche");
+
+} else{
+    console.log("valido");
+}
+
+
+
+const euroKm = (0.21).toFixed(2);
+
+let prezzoBiglietto = distanzaKm * euroKm;
+
+const prezzoBigliettoUnder18= (distanzaKm * euroKm) * 0.2;
+const prezzoBigliettoOver65= (distanzaKm * euroKm) * 0.4;
+
+
+
+if (etaPasseggero < 18){
+
+    alert("Complimenti! Hai diritto uno sconto del 20%. Il prezzo finale da pagare è" + prezzoBigliettoUnder18.toFixed(2) + "$" );
+
+} else if (etaPasseggero > 65){
+
+    alert("Complimenti! Hai diritto a uno sconto del 40% .Il prezzo finale da pagare è" + prezzoBigliettoOver65.toFixed(2) + "$");
+} else{
+    alert( "Complimeti, il totale del suo biglietto è di" + prezzoBiglietto.toFixed(2) + "$")
+}
+
+
+
+
+
+
+
+
+
